640aeedbaed1564962b3cad653d528b0eb8b5902
Sarbast Hesso <sarbast.hesso@gmail.com>
2025-05-15 13:41:21 +0200

    Added Controllers and Views to Index and create

---
+++ EmployeeApp.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeApp.Web.Models;
+using EmployeeApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static EmployeeService employeeService = new EmployeeService();
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var model = employeeService.GetAll();
+            return View(model);
+        }
+
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            employeeService.AddEmployee(employee);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
