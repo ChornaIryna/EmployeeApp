87d05d2900d9eb037b64f8d4d66df24fe48baaa4
Iryna <iryna.chorna@hotmail.com>
2025-05-15 11:28:07 +0200

    Added Models

---
+++ EmployeeApp.Web/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApp.Web.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Enter a Name")]
+    [Display(Name = "Name", Prompt = "Employee name")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Enter en Email")]
+    [EmailAddress(ErrorMessage = "Enter a valid email")]
+    [Display(Name = "Email", Prompt = "Employee email")]
+    public string? Email { get; set; }
+}
