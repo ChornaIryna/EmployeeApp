6870bde62a67ef25eea0ff34b2dcbb8b51649288
Iryna <iryna.chorna@hotmail.com>
2025-05-15 14:33:30 +0200

    Added Details

--- EmployeeApp.Web/Controllers/EmployeesController.cs
+++ EmployeeApp.Web/Controllers/EmployeesController.cs
@@ -2,34 +2,45 @@
 using EmployeeApp.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EmployeeApp.Web.Controllers
+namespace EmployeeApp.Web.Controllers;
+
+public class EmployeesController : Controller
 {
-    public class EmployeesController : Controller
+    private static EmployeeService employeeService = new EmployeeService();
+
+    [HttpGet("")]
+    public IActionResult Index()
     {
-        private static EmployeeService employeeService = new EmployeeService();
+        var model = employeeService.GetAll();
+        return View(model);
+    }
 
-        [HttpGet("")]
-        public IActionResult Index()
-        {
-            var model = employeeService.GetAll();
-            return View(model);
-        }
+    [HttpGet("create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-        [HttpGet("Create")]
-        public IActionResult Create()
+    [HttpPost("create")]
+    public IActionResult Create(Employee employee)
+    {
+        if (!ModelState.IsValid)
         {
             return View();
         }
-
-        [HttpPost("Create")]
-        public IActionResult Create(Employee employee)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-            employeeService.AddEmployee(employee);
-            return RedirectToAction(nameof(Index));
-        }
+        employeeService.AddEmployee(employee);
+        return RedirectToAction(nameof(Index));
     }
+
+    [HttpGet("details/{id}")]
+    public IActionResult Details(int id)
+    {
+        var employee = employeeService.GetById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        return View(employee);
+    }
+
 }
---
+++ EmployeeApp.Web/NameStringAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApp.Web;
+
+public class NameStringAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value) => !value.ToString().Contains('@');
+
+}
